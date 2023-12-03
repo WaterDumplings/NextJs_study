e0b0280d5b0231d5553484188ee543ca4bdfffcb
김성준 <q881229@gmail.com>
2023-12-03 18:30:03 +0900

    props, useState, onClick, array, object state 변경 공부

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,28 +3,40 @@ import { age, name } from './data';
 import Hi from './hi';
 
 export default function Cart() {
+  let 장바구니 : string[] = ['Tomatoes', 'Pasta']
+
   return (
     <div>
       <h4 className="title">Cart</h4>
-      {
-        <Hi/>
-      }
       <div className="cart-item">
-        <p>상품명 {age}</p>
+        <p>상품명</p>
         <p>$40</p>
         <p>1개</p>
       </div>
-      <CartItem />
+      <CartItem item = {장바구니[0]} />
+      <CartItem item = {장바구니[1]} />
+      <Banner content = "롯데카드" />
+      <Banner content = "현대카드" />
+      <Button color = 'blue' />
     </div>
   )
 }
+
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>
+}
+
+function Button(props) {
+  return <button style={{color : props.color}} > 하이 </button>
+}
+
 
 // server component -> 아무데나 만든거 : 로딩 속도가 빠르다.
 function CartItem(props) {
   return (
     <div>
       <div className="cart-item">
-        <p>상품명</p>
+        <p>{props.item}</p>
         <p>$40</p>
         <p>1개</p>
       </div>
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,19 +1,34 @@
-import Image from "next/image";;
+'use client'
+
+import Image from "next/image";
+import { useState } from "react";
 
 export default function List() {
-  let 상품 : string[] = ['Tomatoes', 'Pasta', 'Coconut'];
-  let array :number[] = [2, 3, 4]
+  let 상품: string[] = ['Tomatoes', 'Pasta', 'Coconut'];
+  let array: number[] = [2, 3, 4]
+  let [수량, 수량변경] = useState([0, 0, 0])
 
   return (
     <div>
-      <h2>Products</h2>
+      <h4 className="title">상품목록</h4>
       {
         상품.map((a, i) => {
           return (
-          <div className="food" key = {i}>
-            <img src={`food${i}.png`} alt="" className="food-img" />
-            <h4>{a} $40</h4>
-          </div>
+            <div className="food" key={i}>
+              <img src={`food${i}.png`} alt="" className="food-img" />
+              <h4>{a} $40</h4>
+              <span> {수량[i]} </span>
+              <button onClick={() => {
+                let copy: number[] = [...수량]
+                copy[i]++
+                수량변경(copy)
+              }}>+</button>
+              <button onClick={() => {
+                let copy: number[] = [...수량]
+                copy[i]--
+                수량변경(copy)
+              }}>-</button>
+            </div>
           )
         })
       }
